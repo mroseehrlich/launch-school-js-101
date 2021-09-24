27fdf2d522da113a77580e9832a44812a590ef01
Mia Ehrlich <m.rose.ehrlich@gmail.com>
2021-09-24 11:31:32 -0400

    add psuedocode exercies

---
+++ lesson_2/pseudocode.js
@@ -0,0 +1,40 @@
+/*
+** 1) a function that returns the sum of two numbers
+
+**  START
+**  SET number 1
+**  SET number 2
+**  SET function with two numbers as arguments
+**  Return number1 + number 2
+**  END
+** 
+** 2) a function that takes an array of strings, and returns a string that is all those strings concatenated together
+** 
+**  START
+**  SET strings = array of strings
+**  SET iterator = 1
+**  SET function with strings as argument
+**  
+**  IF strings != undefined
+**    WHILE iterator <= length of strings
+**      SET currentString = value of string at strings collection of iterator
+**      SET fullString = empty string
+**      fullString = fullString + currentString
+**      iterator = iterator + 1
+**   
+**    return fullString
+**
+**  END
+** 
+**  3) a function that takes an array of integers, and returns a new array with every other element
+**  START
+**  SET integers = array of integers
+**  SET iterator = 1
+**  IF integers !== undefined
+**    SET newArray = empty array
+**    WHILE iterator <= length of integers
+**      Add integer value at iterator to newArray
+**      iterator = iterator + 2
+**    Return newArray
+**  END
+*/
