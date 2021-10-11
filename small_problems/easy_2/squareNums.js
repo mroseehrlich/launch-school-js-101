d50caef664659d723fb9d5cfad64358824e012d7
Mia Ehrlich <m.rose.ehrlich@gmail.com>
2021-10-11 17:31:50 -0400

    add recursive fu ction to calculate power of the nth

--- small_problems/easy_2/squareNums.js
+++ small_problems/easy_2/squareNums.js
@@ -2,5 +2,15 @@ const multiply = (num1, num2) => num1 * num2;
 
 const square = (num) => multiply(num, num);
 
+const power = (num, exp) => {
+  if (exp === 1) {
+    return multiply(num, 1);
+  }
+
+  return num * power(num, exp - 1);
+};
+
 console.log(square(5) === 25);
 console.log(square(-8) === 64);
+
+console.log(power(3, 3));
