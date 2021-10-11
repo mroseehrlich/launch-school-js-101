d958c560622d964891c9b81407d9317204407b83
Mia Ehrlich <m.rose.ehrlich@gmail.com>
2021-10-11 16:27:04 -0400

    check if year is leap year

---
+++ small_problems/easy_1/leapYears_pt1.js
@@ -0,0 +1,22 @@
+const isLeapYear = (year) => {
+  if (year % 100 === 0) {
+    return year % 400 === 0;
+  } else {
+    return year % 4 === 0;
+  }
+};
+
+isLeapYear(2016);
+isLeapYear(2015);
+isLeapYear(2100);
+isLeapYear(2400);
+isLeapYear(240000);
+isLeapYear(240001);
+isLeapYear(2000);
+isLeapYear(1900);
+isLeapYear(1752);
+isLeapYear(1700);
+isLeapYear(1);
+isLeapYear(100);
+isLeapYear(400);
+
