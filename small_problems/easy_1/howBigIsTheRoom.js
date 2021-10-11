79080116f3f611857c49f97376dbfaaf1f4b206c
Mia Ehrlich <m.rose.ehrlich@gmail.com>
2021-10-11 13:15:04 -0400

    update program to prompt user for measurement type

--- small_problems/easy_1/howBigIsTheRoom.js
+++ small_problems/easy_1/howBigIsTheRoom.js
@@ -1,14 +1,29 @@
 const readline = require("readline-sync");
 
-const SQMETERS_TO_SQFEET = 10.7639;
+//const SQMETERS_TO_SQFEET = 10.7639;
 
-// GET input for length in meters
-let length = Number(readline.prompt("Enter the length of the room in meters:"));
-// GET input for width in meters
-let width = Number(readline.prompt("Enter the width of the room in meters:"));
+// PROMPT input for either feet or meters
+console.log('Would you like to measure in feet or meters? Enter "meters" or "feet"');
+let measurementType = readline.prompt().toLowerCase();
+
+while (measurementType !== "feet" && measurementType !== "meters") {
+  console.log('That doesn\'t look like a valid type of measurement. Please enter "feet" or "meters"');
+  measurementType = readline.prompt().toLowerCase();
+}
+
+// PROMPT input for length in meters
+console.log("Enter the length of the room in meters:");
+let length = Number(readline.prompt());
+// PROMPT input for width in meters
+console.log("Enter the width of the room in meters:");
+let width = Number(readline.prompt());
+
+// CALCULATE area
+//let areaInSquareMeters = length * width;
+// let areaInSquareFeet = areaInSquareMeters * SQMETERS_TO_SQFEET;
+let area = length * width;
 
 // PRINT length * width in square meters and square feet
-let areaInSquareMeters = length * width;
-let areaInSquareFeet = areaInSquareMeters * SQMETERS_TO_SQFEET;
-
-console.log(`The area of the room is ${areaInSquareMeters.toFixed(2)} square meters (${areaInSquareFeet.toFixed(2)} square feet)`);
+//console.log(`The area of the room is ${areaInSquareMeters} square meters
+//  (${areaInSquareFeet} square feet).`);
+console.log(`The area of the room is ${area.toFixed(2)} square ${measurementType}`);
