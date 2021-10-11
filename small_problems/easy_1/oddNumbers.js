886a890f30b51d6a33a2db17453bad538dc2ef5a
Mia Ehrlich <m.rose.ehrlich@gmail.com>
2021-10-11 11:10:00 -0400

    add three solutions for logging odd numbers between 1 and 99

---
+++ small_problems/easy_1/oddNumbers.js
@@ -0,0 +1,18 @@
+// for (let number = 1; number < 100; number += 2) {
+//   console.log(number);
+// }
+
+// further exploration
+// for (let num = 1; num < 100; num++) {
+//   if (num % 2 === 0) continue;
+//
+//   console.log(num);
+// }
+
+let count = 1;
+
+while (count < 100) {
+  console.log(count);
+
+  count += 2;
+}
