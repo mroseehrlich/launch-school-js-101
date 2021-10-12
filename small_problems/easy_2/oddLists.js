f417914a2ff1d4ddcf98842908146d2ff47241d8
Mia Ehrlich <m.rose.ehrlich@gmail.com>
2021-10-12 16:27:12 -0400

    write functions that return odd index elements from an array and a second fourth and sixth elements from an array

---
+++ small_problems/easy_2/oddLists.js
@@ -0,0 +1,30 @@
+const oddities = (array) => {
+  let oddIndex = array.filter((value, index) => {
+    if (index % 2 === 0 ) {
+      return value;
+    }
+  });
+  return oddIndex;
+};
+
+const secondFourthSixth = (array) => {
+  let elements = [];
+  for (let index = 0; index < array.length; index += 1) {
+    if (index === 1 || index === 3 || index === 5) {
+      elements.push(array[index]);
+    }
+  }
+  return elements;
+};
+
+console.log(oddities([2, 3, 4, 5, 6])); // logs [2, 4, 6]
+console.log(oddities([1, 2, 3, 4, 5, 6])); // logs [1, 3, 5]
+console.log(oddities(["abc", "def"])); // logs ['abc']
+console.log(oddities([123])); // logs [123]
+console.log(oddities([])); // logs []
+
+console.log(secondFourthSixth([2, 3, 4, 5, 6])); // logs [2, 4, 6]
+console.log(secondFourthSixth([1, 2, 3, 4, 5, 6])); // logs [1, 3, 5]
+console.log(secondFourthSixth(["abc", "def"])); // logs ['abc']
+console.log(secondFourthSixth([123])); // logs [123]
+console.log(secondFourthSixth([])); // logs []
