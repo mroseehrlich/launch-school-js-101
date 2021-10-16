474ac25d87e81c26b9c98ff8c129c0991c0427f5
Mia Ehrlich <m.rose.ehrlich@gmail.com>
2021-10-16 14:54:17 -0400

    write function that takes a string as an argument and returns an object with keys for word length and valuers for numbert of words with that length

---
+++ small_problems/easy_4/letterCounter_pt1.js
@@ -0,0 +1,24 @@
+const wordSizes = (string) => {
+
+  let words = string.split(" ");
+  let sizes = {};
+
+  words.forEach(word => {
+    if (word.length === 0) return;
+
+    if (!sizes[word.length]) {
+      sizes[word.length] = 0;
+    }
+
+    sizes[word.length] += 1;
+
+  });
+
+
+  console.log(sizes);
+};
+
+wordSizes('Four score and seven.');                       // { "3": 1, "4": 1, "5": 1, "6": 1 }
+wordSizes('Hey diddle diddle, the cat and the fiddle!');  // { "3": 5, "6": 1, "7": 2 }
+wordSizes("What's up doc?");                              // { "2": 1, "4": 1, "6": 1 }
+wordSizes('');                                            // {}
