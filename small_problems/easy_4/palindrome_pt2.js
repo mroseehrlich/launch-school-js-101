c0a8d9ff5bd7e555af2025dd32fcdcd4c7ddc3cd
Mia Ehrlich <m.rose.ehrlich@gmail.com>
2021-10-16 13:45:19 -0400

    check palidrome with non-alphanumerics removed and insensitive of case

---
+++ small_problems/easy_4/palindrome_pt2.js
@@ -0,0 +1,11 @@
+const isRealPalindrome = string => {
+  let cleanString = string.toLowerCase().replace(/[^a-z0-9]/g, '');
+  return cleanString === [...cleanString].reverse().join('');
+};
+
+console.log(isRealPalindrome('madam'));               // true
+console.log(isRealPalindrome('Madam'));               // true (case does not matter)
+console.log(isRealPalindrome("Madam, I'm Adam"));     // true (only alphanumerics matter)
+console.log(isRealPalindrome('356653'));              // true
+console.log(isRealPalindrome('356a653'));             // true
+console.log(isRealPalindrome('123ab321'));            // false
