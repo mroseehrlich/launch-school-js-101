75cfe3f18af2f595b3715fd472f38702a27c7e24
Mia Ehrlich <m.rose.ehrlich@gmail.com>
2021-10-17 18:27:16 -0400

    write function that takes an integer as an argument and returns an array of all digits in the number

---
+++ small_problems/easy_5/listOfDigits.js
@@ -0,0 +1,16 @@
+const digitList = integer => {
+//   const stringOfInt = `${integer}`;
+//   let list = [];
+//
+//   for (let idx = 0; idx < stringOfInt.length; idx += 1) {
+//     list.push(Number(stringOfInt[idx]));
+//   }
+//   console.log(list);
+
+  return [...`${integer}`].map(value => parseInt(value, 10));
+};
+
+console.log(digitList(12345));       // [1, 2, 3, 4, 5]
+console.log(digitList(7));           // [7]
+console.log(digitList(375290));      // [3, 7, 5, 2, 9, 0]
+console.log(digitList(444));         // [4, 4, 4]
