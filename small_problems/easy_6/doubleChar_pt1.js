7988757fe165d50b3b47397ac9a80bdab96f35c3
Mia Ehrlich <m.rose.ehrlich@gmail.com>
2021-10-18 12:47:22 -0400

    write function that takes a string and return a new string with each character from the original string repeated twice

---
+++ small_problems/easy_6/doubleChar_pt1.js
@@ -0,0 +1,5 @@
+const repeater = string => [...string].map(char => char + char).join('');
+
+console.log(repeater('Hello'));        // "HHeelllloo"
+console.log(repeater('Good job!'));    // "GGoooodd  jjoobb!!"
+console.log(repeater(''));             // ""
