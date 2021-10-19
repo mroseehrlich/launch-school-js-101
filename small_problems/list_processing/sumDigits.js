4d8321991fb518231e970baa26c388e664401173
Mia Ehrlich <m.rose.ehrlich@gmail.com>
2021-10-19 11:23:08 -0400

    write function that takes a positive integer and returns the sum of its digits

---
+++ small_problems/list_processing/sumDigits.js
@@ -0,0 +1,5 @@
+const sum = integer => [...`${integer}`].reduce((sum, digit) => sum + Number(digit), 0);
+
+console.log(sum(23));           // 5
+console.log(sum(496));          // 19
+console.log(sum(123456789));    // 45
