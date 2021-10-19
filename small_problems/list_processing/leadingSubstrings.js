111a1407edb883f5b90cb91096827dad434930a9
Mia Ehrlich <m.rose.ehrlich@gmail.com>
2021-10-19 12:43:16 -0400

    update with map and reduce methods

--- small_problems/list_processing/leadingSubstrings.js
+++ small_problems/list_processing/leadingSubstrings.js
@@ -1,11 +1,17 @@
+// const leadingSubstrings = string => {
+//   let substrings = [];
+//
+//   for (let stringLength = 1; stringLength <= string.length; stringLength += 1) {
+//     substrings.push(string.slice(0, stringLength));
+//   }
+//
+//   return substrings;
+// };
+
 const leadingSubstrings = string => {
-  let substrings = [];
-
-  for (let stringLength = 1; stringLength <= string.length; stringLength += 1) {
-    substrings.push(string.slice(0, stringLength));
-  }
-
-  return substrings;
+  return [...string].map((char, index) => {
+    return [...string].slice(0, index + 1).reduce((subString, char) => subString + char, '');
+  });
 };
 
 console.log(leadingSubstrings('abc'));      // ["a", "ab", "abc"]
