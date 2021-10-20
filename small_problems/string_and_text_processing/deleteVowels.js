0700044b9426923d9fc4ac764eec150fad5c4e2c
Mia Ehrlich <m.rose.ehrlich@gmail.com>
2021-10-20 16:24:03 -0400

    Write a function that takes an array of strings and returns an array of the same string values, but with all vowels (a, e, i, o, u) removed.

---
+++ small_problems/string_and_text_processing/deleteVowels.js
@@ -0,0 +1,9 @@
+const removeVowels = strings => {
+  return strings.map(string => {
+    return string.replace(/[aeiou]/gi, '');
+  });
+};
+
+console.log(removeVowels(['abcdefghijklmnopqrstuvwxyz']));         // ["bcdfghjklmnpqrstvwxyz"]
+console.log(removeVowels(['green', 'YELLOW', 'black', 'white']));  // ["grn", "YLLW", "blck", "wht"]
+console.log(removeVowels(['ABC', 'AEIOU', 'XYZ']));                // ["BC", "", "XYZ"]
