aaf57d04c8b42eaf200cdd0fcf216bb367632cd0
Mia Ehrlich <m.rose.ehrlich@gmail.com>
2021-10-20 16:02:27 -0400

    Building on the previous exercise, write a function that returns true or false based on whether or not an inventory item is available. As before, the function takes two arguments: an inventory item and a list of transactions. The function should return true only if the sum of the quantity values of the item's transactions is greater than zero. Notice that there is a movement property in each transaction object. A movement value of 'out' will decrease the item's quantity.

---
+++ small_problems/list_processing/inventoryItemAvailability.js
@@ -0,0 +1,30 @@
+const transactionsFor = (itemId, transactions) => {
+  return transactions.filter(transaction => transaction.id === itemId);
+};
+
+const isItemAvailable = (itemId, transactions) => {
+  let quantity = transactionsFor(itemId, transactions).reduce((sum, item) => {
+    if (item.movement === 'in') {
+      return sum + item.quantity;
+    } else {
+      return sum - item.quantity;
+    }
+  }, 0);
+
+  return quantity > 0;
+};
+
+let transactions = [ { id: 101, movement: 'in',  quantity:  5 },
+  { id: 105, movement: 'in',  quantity: 10 },
+  { id: 102, movement: 'out', quantity: 17 },
+  { id: 101, movement: 'in',  quantity: 12 },
+  { id: 103, movement: 'out', quantity: 20 },
+  { id: 102, movement: 'out', quantity: 15 },
+  { id: 105, movement: 'in',  quantity: 25 },
+  { id: 101, movement: 'out', quantity: 18 },
+  { id: 102, movement: 'in',  quantity: 22 },
+  { id: 103, movement: 'out', quantity: 15 }, ];
+
+console.log(isItemAvailable(101, transactions));     // false
+console.log(isItemAvailable(103, transactions));     // false
+console.log(isItemAvailable(105, transactions));     // true
