fc7bb2f7582bf2fea0de74588139febf75b76389
Mia Ehrlich <m.rose.ehrlich@gmail.com>
2021-10-20 19:44:09 -0400

    update solution using a regex that checks word boundaries at the beginning and end of the matched regex

--- small_problems/string_and_text_processing/searchWord_pt1.js
+++ small_problems/string_and_text_processing/searchWord_pt1.js
@@ -1,8 +1,10 @@
+/*
 const searchWord = (word, text) => {
   let count = 0;
 
   text.split(' ').forEach(wordInText => {
-    let cleanWord = [...wordInText].filter(char => /[a-z]/gi.test(char)).join('');
+    let cleanWord = [...wordInText].filter(char => /[a-z]/gi.test(char))
+      .join('');
     console.log(cleanWord);
     if (cleanWord.toLowerCase() === word.toLowerCase()) {
       count += 1;
@@ -11,7 +13,15 @@ const searchWord = (word, text) => {
 
   return count;
 };
+*/
 
-const text = 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?';
+const searchWord = (word, text) => {
+  let regex = new RegExp(`\\b${word}\\b`, 'gi');
+  let matches = text.match(regex);
+
+  return matches ? matches.length : 0;
+};
+
+const text = 'Sed sedut ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?';
 
 console.log(searchWord('sed', text));      // 3
