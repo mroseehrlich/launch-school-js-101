50e13e1e0549a65df16b009b67986f9dd938c72e
Mia Ehrlich <m.rose.ehrlich@gmail.com>
2021-10-20 17:21:29 -0400

    Write a function that takes a string as an argument and returns that string with a staggered capitalization scheme. Every other character, starting from the first, should be capitalized and should be followed by a lowercase or non-alphabetic character. Non-alphabetic characters should not be changed, but should be counted as characters for determining when to switch between upper and lower case.

---
+++ small_problems/string_and_text_processing/staggeredCaps.js
@@ -0,0 +1,17 @@
+const staggeredCase = string => {
+  return [...string].map((letter, index) => {
+    if (/[A-Za-z]/.test(letter)) {
+      if (index % 2 === 0) {
+        return letter.toUpperCase();
+      } else {
+        return letter.toLowerCase();
+      }
+    } else {
+      return letter;
+    }
+  }).join('');
+};
+
+console.log(staggeredCase('I Love Launch School!'));        // "I LoVe lAuNcH ScHoOl!"
+console.log(staggeredCase('ALL_CAPS'));                     // "AlL_CaPs"
+console.log(staggeredCase('ignore 77 the 4444 numbers'));   // "IgNoRe 77 ThE 4444 nUmBeRs"
