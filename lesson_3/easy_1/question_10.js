9c54fc11c6cc698454e04bc9a8106064446693fe
Mia Ehrlich <m.rose.ehrlich@gmail.com>
2021-10-25 11:44:01 -0400

    Return a new version of this sentence that ends just before the word house. Don't worry about spaces or punctuation: remove everything starting from the beginning of house to the end of the sentence.

---
+++ lesson_3/easy_1/question_10.js
@@ -0,0 +1,5 @@
+let advice = "Few things in life are as important as house training your pet dinosaur.";
+
+// Expected return value:
+// => 'Few things in life are as important as '
+advice.slice(0, advice.indexOf('house'));
