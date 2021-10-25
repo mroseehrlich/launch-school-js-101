cbaf82789b4e66bcfb52594365dd16ecde002f69
Mia Ehrlich <m.rose.ehrlich@gmail.com>
2021-10-25 17:37:34 -0400

    Remove console.log()

--- lesson_3/medium_1/question_2.js
+++ lesson_3/medium_1/question_2.js
@@ -1,8 +1,8 @@
 let munstersDescription = "The Munsters are creepy and spooky.";
-console.log([...munstersDescription].map(char => {
+[...munstersDescription].map(char => {
   if (char === char.toUpperCase()) {
     return char.toLowerCase();
   } else {
     return char.toUpperCase();
   }
-}).join(''));
+}).join('');
