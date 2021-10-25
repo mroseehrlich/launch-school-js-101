2b3ae0d9067cdde2616a6a8aade1256b880f4fb4
Mia Ehrlich <m.rose.ehrlich@gmail.com>
2021-10-25 16:31:25 -0400

    Write two one line expressions to count the number of lower-case t's in two strings

---
+++ lesson_3/easy_2/question_10.js
@@ -0,0 +1,6 @@
+let statement1 = "The Flintstones Rock!";
+let statement2 = "Easy come, easy go.";
+
+[...statement1].filter(char => char === 't').length;
+[...statement2].filter(char => char === 't').length;
+
