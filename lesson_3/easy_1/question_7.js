266b37daac5024c7687bf86a22af00bb6642e39f
Mia Ehrlich <m.rose.ehrlich@gmail.com>
2021-10-25 11:36:56 -0400

    Two solutions that check whether or not a given substring appears in a string

---
+++ lesson_3/easy_1/question_7.js
@@ -0,0 +1,5 @@
+let str1 = "Few things in life are as important as house training your pet dinosaur.";
+let str2 = "Fred and Wilma have a pet dinosaur named Dino.";
+
+str1.indexOf('Dino') !== -1;
+str2.includes('Dino');
