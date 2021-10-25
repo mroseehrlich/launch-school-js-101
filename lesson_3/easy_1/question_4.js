feb5561135a9dd6b74e3b4a2cb39aa44d38b39d5
Mia Ehrlich <m.rose.ehrlich@gmail.com>
2021-10-25 11:25:23 -0400

    Use a given string to create a new string with the first char capitalized and all other characters lowercase

---
+++ lesson_3/easy_1/question_4.js
@@ -0,0 +1,3 @@
+let munstersDescription = "the Munsters are CREEPY and Spooky.";
+// => The munsters are creepy and spooky.
+munstersDescription.charAt(0).toUpperCase().concat(munstersDescription.slice(1).toLowerCase());
