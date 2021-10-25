2ffe97d68d1aedc523cd8b800fee9cb7a7c4e7f2
Mia Ehrlich <m.rose.ehrlich@gmail.com>
2021-10-25 17:56:30 -0400

    Explain the difference between using concat and push in two different implementations of a rolling buffer function

---
+++ lesson_3/medium_1/question_4.js
@@ -0,0 +1,4 @@
+// Yes, there is a difference between using .concat() and .push() in
+// the two implementations. The .push() method
+// mutates the array argument given to the calling function while .concat()
+// does not.
