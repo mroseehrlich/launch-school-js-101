6a917d86e2a4e8a59ad1c114f6e06ae4119296cc
Mia Ehrlich <m.rose.ehrlich@gmail.com>
2021-10-25 11:24:28 -0400

    Check if object contains an entry with a given value

---
+++ lesson_3/easy_1/question_3.js
@@ -0,0 +1,7 @@
+let ages = { Herman: 32, Lily: 30, Grandpa: 402, Eddie: 10 };
+
+ages.hasOwnProperty('Spot');
+
+// You can use Object.prototype.hasOwnProperty() to determine if
+// an object contains a particular key/value entry
+
