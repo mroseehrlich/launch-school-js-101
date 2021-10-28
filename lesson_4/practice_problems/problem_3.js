c06be4dbd53511603b53788d52a0502ac39fa040
Mia Ehrlich <m.rose.ehrlich@gmail.com>
2021-10-28 19:17:35 -0400

    Evaluate code for a map method statement that uses an explicit return in a single line arrow function callback

---
+++ lesson_4/practice_problems/problem_3.js
@@ -0,0 +1,5 @@
+[1, 2, 3].map(num => num * num);
+
+// This code returns a new array containing [1, 4, 9].
+// This happens because arrow functions allow for an explicit return
+// on single line expressions when no braces are used.
