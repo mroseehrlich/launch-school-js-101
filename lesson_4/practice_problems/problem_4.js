b111b496dc2b55bf86955361c77622919d2728c1
Mia Ehrlich <m.rose.ehrlich@gmail.com>
2021-10-28 19:19:54 -0400

    Evaluate code that chains .pop() with .length

---
+++ lesson_4/practice_problems/problem_4.js
@@ -0,0 +1,4 @@
+['ant', 'bear', 'caterpillar'].pop().length;
+
+// This code returns 11 because the return value after .pop()
+// is the string caterpillar and .length evaluates the length of the string.
