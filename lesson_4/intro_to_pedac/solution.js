3eb92cab4ecc194cb0a6d6f22fa116154b657147
Mia Ehrlich <m.rose.ehrlich@gmail.com>
2021-10-28 12:55:22 -0400

    Soultion code for sumEvenNumberRow function from PEDAC intro video tutorial for code implementation

---
+++ lesson_4/intro_to_pedac/solution.js
@@ -0,0 +1,43 @@
+function sumEvenNumberRow(rowNumber) {
+  let rows = [];
+  let startInteger = 2;
+
+  for (let currentRowNum = 1; currentRowNum <= rowNumber; currentRowNum += 1) {
+    let row = createRow(startInteger, currentRowNum);
+    rows.push(row);
+    startInteger = row[row.length - 1] + 2;
+  }
+
+  let finalRow = rows.pop();
+  let sum = finalRow.reduce((sum, num) => sum + num, 0);
+  return sum;
+}
+
+function createRow(startInteger, rowLength) {
+  const row = [];
+  let currentInteger = startInteger;
+
+  while (row.length < rowLength) {
+    row.push(currentInteger);
+    currentInteger += 2;
+  }
+
+  return row;
+}
+
+// Sum even number row
+// row number: 1 --> sum of integers in row: 2
+// row number: 2 --> sum of integers in row: 10
+// row number: 4 --> sum of integers in row: 68
+
+console.log(sumEvenNumberRow(1)); // 2
+console.log(sumEvenNumberRow(2)); // 10
+console.log(sumEvenNumberRow(4)); // 68
+
+// Create Row
+// start 2, length: 1 --> [2]
+// start 4, length: 2 --> [4, 6]
+// start 8, length: 3 --> [8, 10, 12]
+console.log(createRow(2, 1)); // [2]
+console.log(createRow(4, 2)); // [4, 6]
+console.log(createRow(8, 3)); // [8, 10, 12]
