4a2576571c1e035ed885f56a191f67d5a84e1b3c
Mia Ehrlich <m.rose.ehrlich@gmail.com>
2021-11-08 19:53:16 -0500

    Tranpose matrixes of n colunms or rows

---
+++ small_problems/advanced_1/matrix_2.js
@@ -0,0 +1,33 @@
+const transpose = matrix => {
+  let newMatrix = [];
+
+  for (let row = 0; row < matrix[0].length; row++) {
+    newMatrix.push([]);
+  }
+
+  for (let rowIdx = 0; rowIdx < matrix.length; rowIdx++) {
+    for (let columnIdx = 0; columnIdx < matrix[rowIdx].length; columnIdx++) {
+      newMatrix[columnIdx].push(matrix[rowIdx][columnIdx]);
+    }
+  }
+
+  return newMatrix;
+};
+
+const matrix = [
+  [1, 5, 8, 5],
+  [4, 7, 2, 0],
+  [3, 9, 6, 1]
+];
+
+let newMatrix = transpose(matrix);
+
+console.log(newMatrix);      // [[1, 4, 3], [5, 7, 9], [8, 2, 6], [5, 0, 1]]
+console.log(matrix);         // [[1, 5, 8, 5], [4, 7, 2, 0], [3, 9, 6, 1]]
+
+console.log(transpose([[1, 2, 3, 4]]));            // [[1], [2], [3], [4]]
+console.log(transpose([[1], [2], [3], [4]]));      // [[1, 2, 3, 4]]
+console.log(transpose([[1]]));                     // [[1]]
+
+console.log(transpose([[1, 2, 3, 4, 5], [4, 3, 2, 1, 0], [3, 7, 8, 6, 2]]));
+// [[1, 4, 3], [2, 3, 7], [3, 2, 8], [4, 1, 6], [5, 0, 2]]
