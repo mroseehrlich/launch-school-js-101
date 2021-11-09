2be8deee6a5bcae4eb5626d26a4acb80be54d2b4
Mia Ehrlich <m.rose.ehrlich@gmail.com>
2021-11-08 19:40:50 -0500

    Add function that takes an array of arrays that represents a 3x3 matrix and returns the transpose of the matrix

---
+++ small_problems/advanced_1/matrix.js
@@ -0,0 +1,22 @@
+const transpose = matrix => {
+  let newMatrix = [[], [], []];
+
+  for (let rowIdx = 0; rowIdx < matrix.length; rowIdx++) {
+    for (let columnIdx = 0; columnIdx < matrix[rowIdx].length; columnIdx++) {
+      newMatrix[rowIdx].push(matrix[columnIdx][rowIdx]);
+    }
+  }
+
+  return newMatrix;
+};
+
+const matrix = [
+  [1, 5, 8],
+  [4, 7, 2],
+  [3, 9, 6]
+];
+
+let newMatrix = transpose(matrix);
+
+console.log(newMatrix);      // [[1, 4, 3], [5, 7, 9], [8, 2, 6]]
+console.log(matrix);         // [[1, 5, 8], [4, 7, 2], [3, 9, 6]]
